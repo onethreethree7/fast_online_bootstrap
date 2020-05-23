3ff4317d0148f1a78ca5725e572b68b4bef789d2
Laurence Sebastian Bowes <laurencebowes@gmail.com>
2020-05-23 14:11:29 +0100

    Initial commit of code

---
+++ bootstrap.c
@@ -0,0 +1,48 @@
+#include <math.h>
+#include <stdio.h>
+#include "bootstrap.h"
+
+b_instance* b_setup(int num, gsl_rng* rng) {
+	b_instance* inst = malloc(sizeof(b_instance));
+	inst->rng = rng;
+	inst->num = num;
+	inst->mean = 0;
+	inst->error = 0;
+	inst->samples = 0;
+	inst->bins = calloc(num, sizeof(b_bin));
+	return inst;
+}
+
+void b_add(b_instance* inst, double val) {
+	int poiss;
+	double mean = 0;
+	b_bin* curbin;
+	for(int j = 0; j < inst->num; j++) {
+		curbin = &inst->bins[j];
+		poiss = gsl_ran_poisson(inst->rng, 1.0);
+		for(int k = 0; k < poiss; k++) {
+		    curbin->mean = (curbin->mean*curbin->size + val)/((curbin->size) + 1);
+		    curbin->size += 1;
+		}
+		mean += curbin->mean;
+	}
+	inst->mean = mean/inst->num;
+	inst->samples++;
+	return;
+}
+
+void b_upderr(b_instance* inst) {
+	double calc = 0;
+	for (int i = 0; i < inst->num; i++) {
+		calc += (inst->bins[i].mean - inst->mean)*(inst->bins[i].mean - inst->mean);
+	}
+	calc /= (inst->num - 1);
+	inst->error = sqrt(calc);
+	return;
+}
+
+void b_clean(b_instance* inst) {
+	free(inst->bins);
+	free(inst);
+	return;
+}
---
+++ bootstrap.h
@@ -0,0 +1,27 @@
+#ifndef BOOTSTRAP_H
+#define BOOTSTRAP_H
+
+#include <gsl/gsl_randist.h>
+
+struct b_bin {
+    unsigned long size;
+    double mean;
+};
+typedef struct b_bin b_bin;
+
+struct b_instance {
+    gsl_rng* rng;
+    int num;
+    unsigned long long samples;
+    b_bin* bins;
+    double mean;
+    double error;
+};
+typedef struct b_instance b_instance;
+
+b_instance* b_setup(int, gsl_rng*);
+void b_add(b_instance*, double);
+void b_upderr(b_instance*);
+void b_clean(b_instance*);
+
+#endif
